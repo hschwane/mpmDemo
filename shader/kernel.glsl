b3a70a57bc33737a664ff57b5ea24ccc36de0c9e
Hendrik Schwanekamp <hendrik.schwanekamp@gmx.net>
2019-11-25 14:42:18 +0100

    fix kernel

--- shader/kernel.glsl
+++ shader/kernel.glsl
@@ -1,13 +1,27 @@
 
+
 float N(float x)
 {
     x = abs(x);
-    return (x<1.0f) ? ( 1.0f/2.0f *x*x*x - x*x + 2.0f/3.0f) : ( (x<2.0f) ? (-1.0f/6.0f * x*x*x + x*x - 2*x + 4.0f/3.0f) : 0 );
+    return (x<0.5f) ? (0.75f - x*x) : ( (x<1.5f) ? (0.5f * (1.5f - x*x) ) : 0 );
 }
 
-// cubic bspline kernel
+// quadratic bspline kernel
 float kernel(vec2 x_i, vec2 x_p, float gridLength)
 {
     float hinv = 1.0f / gridLength;
     return N(hinv * (x_p.x - x_i.x)) + N(hinv * (x_p.y - x_i.y));
 }
+
+//float N(float x)
+//{
+//    x = abs(x);
+//    return (x<1.0f) ? ( 1.0f/2.0f *x*x*x - x*x + 2.0f/3.0f) : ( (x<2.0f) ? (-1.0f/6.0f * x*x*x + x*x - 2*x + 4.0f/3.0f) : 0 );
+//}
+//
+//// cubic bspline kernel
+//float kernel(vec2 x_i, vec2 x_p, float gridLength)
+//{
+//    float hinv = 1.0f / gridLength;
+//    return N(hinv * (x_p.x - x_i.x)) + N(hinv * (x_p.y - x_i.y));
+//}
