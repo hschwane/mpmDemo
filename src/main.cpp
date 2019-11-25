509b636a31d1979974cb02181d9c8b634723faec
Hendrik Schwanekamp <hendrik.schwanekamp@gmx.net>
2019-11-25 07:21:17 +0100

    add general user interface

--- src/main.cpp
+++ src/main.cpp
@@ -49,12 +49,22 @@ int main()
                                  solver.setAspectRation(float(width) / float(height));
                              });
 
-    bool simShouldRun = true; //!< should the simulation be running?
+    float backgroundBrushSize = 50;
+    float particleBrushSize = 50;
+    bool simCanRun = true; //!< should the simulation be running?
+    bool simPaused = false; //!< should the simulation be running?
     bool showSolverUI = true; //!< should solver UI be drawn?
+    bool drawGeneralUI = true; //!< draw the general UI window?
+    bool vsync = true; //!< is vsync on?
+    mpu::gph::enableVsync(vsync);
 
     // add some keybindings
     mpu::gph::Input::addButton("ToggleSolverUI","Toggles visibility of the solver ui", [&](mpu::gph::Window& wnd){ showSolverUI = !showSolverUI;});
     mpu::gph::Input::mapKeyToInput("ToggleSolverUI", GLFW_KEY_S);
+    mpu::gph::Input::addButton("TogglePause","Toggles simulation pause state", [&](mpu::gph::Window& wnd){ simPaused = !simPaused;});
+    mpu::gph::Input::mapKeyToInput("TogglePause", GLFW_KEY_P);
+    mpu::gph::Input::addButton("ToggleGeneralUI","Toggles visibility of the general ui", [&](mpu::gph::Window& wnd){ drawGeneralUI = !drawGeneralUI;});
+    mpu::gph::Input::mapKeyToInput("ToggleGeneralUI", GLFW_KEY_G);
 
     // Main loop
     while (window.frameEnd(), mpu::gph::Input::update(), window.frameBegin())
@@ -62,10 +72,34 @@ int main()
         // show imgui demo window
 //        ImGui::ShowDemoWindow();
 
+        if(drawGeneralUI)
+        {
+            if(ImGui::Begin("mpmDemo",&drawGeneralUI))
+            {
+                ImGui::Text("Frametime: %f", mpu::gph::Input::deltaTime());
+                ImGui::Text("FPS: %f", 1.0f / mpu::gph::Input::deltaTime());
+
+                if(ImGui::Checkbox("V-Sync",&vsync))
+                    mpu::gph::enableVsync(vsync);
+
+                if(ImGui::Button((simPaused)? "Continue Simulation" : "Pause Simulation"))
+                    simPaused = !simPaused;
+
+                if(ImGui::Button("Reset Simulation"))
+                {
+                    solver = mpmSolver2D(float(width) / float(height));
+                }
+
+                ImGui::DragFloat("Collision Brush Size",&backgroundBrushSize);
+                ImGui::DragFloat("Particle Brush Size",&backgroundBrushSize);
+            }
+            ImGui::End();
+        }
+
         if(showSolverUI)
             solver.drawUI(&showSolverUI);
 
-        if(simShouldRun)
+        if(simCanRun && !simPaused)
             solver.advanceSimulation();
 
         solver.drawParticles();
